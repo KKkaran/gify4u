6f48812488fcfbb0e4017ab2da6eb94b062dbe47
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-10-01 22:40:19 -0400

    updated results jsx to main to avoid too complicated code

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,14 +12,22 @@ function Main() {
         console.log("finding gifs...")
         //finding new gifs as per the result...
         setLoading(true)
+        console.log(keyword)
+
     }
   return (
     <div className='border border-dark p-3 m-2'>
         <form action="" onSubmit={findGif} className='p-2'>
-            <input type="text" required value={keyword} onChange={(e)=>{setKeyword(e.target.value)}} placeholder='Keyword for gifs...' /> <br /> <br />
+              <input type="text" required value={keyword} onChange={(e) => { setKeyword(e.target.value) }} placeholder='Keyword for gifs...' /> <br /> <br />
             <input type="submit" className='btn btn-info' value='Search Gifs'/>
         </form>
-        {loading ? <Results keyword={keyword}></Results> : <HomeGifs/>}
+        {loading ? <>
+        <div className='border border-dark p-3 m-2'>
+            <h3>Results for {keyword}</h3>
+            <h4>More Results for {keyword} </h4>
+            <h4>More More Results...</h4>
+        </div>
+    </> : <HomeGifs/>}
     </div>
     )
 }
