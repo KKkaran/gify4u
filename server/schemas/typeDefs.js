3586547aac512a3eac9779d8cc3d0ff47496770d
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-10-08 01:47:43 -0400

    created jwt and added to resolvers to authenticate the user

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,8 +9,18 @@ const typeDefs = gql`
         username: String
         email: String
     }
+    type Auth {
+        token: ID!
+        user: User
+    }
     type Query {
         users:[User]
+        me: User
+    }
+
+    type Mutation{
+        login(email:String!, password:String!):Auth
+        addUser(email:String!, username:String!, password:String!):Auth
     }
 `;
 
