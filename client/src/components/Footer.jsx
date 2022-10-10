d494f0d9305eacdbd996c72edf78371c1107fc17
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-10-10 00:30:05 -0400

    footer tag fixed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='bg-dark text-light p-2' style={{ position:'absolute', bottom:0, width:'100%'}}>
-        <h5 className='text-center'>Gify4You &copy; 2022 by Karan Sodhi</h5>
+        <h6 className='text-center'>Gify4You &copy; 2022 by Karan Sodhi</h6>
     </div>
   )
 }
